8d15ab929355d62baa08419b5437626dda877f26
Jason Bailey <jaybailey@outlook.com>
2020-01-18 15:23:05 +0000

    Added login fuctionality to threeauth

--- threeauth/views.py
+++ threeauth/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
+from threeauth.forms import UserLoginForm
 # Create your views here.
 
 def index(request):
@@ -13,4 +14,19 @@ def logout(request):
 
 def login (request):
     #login page
-    return render(request, 'login.html')
+    #login_form = UserLoginForm()
+    if request.method=="POST":
+        login_form = UserLoginForm(request.POST)
+
+        if login_form.is_valid():
+            user = auth.authenticate(username=request.POST['username'],
+                                    password=request.POST['password'])
+
+        if user:
+            auth.login(user=user,request=request)
+            messages.success(request,"You have successfully logged in...")
+        else:
+            login_form.add_error(None,"Your Username or Password was Incorrect")
+    else:
+        login_form = UserLoginForm()
+    return render(request, 'login.html', {"login_form" : login_form})
