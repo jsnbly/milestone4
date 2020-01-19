5cb7c1d050595dcc3f1213ea3d6939694fb964f0
Jason Bailey <jaybailey@outlook.com>
2020-01-19 21:32:34 +0000

    fixed error line 128 static files in settings.py

--- milestone4/settings.py
+++ milestone4/settings.py
@@ -124,8 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+#removed for heroku
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
