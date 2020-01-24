8bc9f868d1f175faba1ecb40b29b19ae2975cace
Jason Bailey <jaybailey@outlook.com>
2020-01-24 06:46:05 +0000

    Upated base.html, views.py and media folder

--- threecart/urls.py
+++ threecart/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path 
+from django.urls import path
+from django.conf.urls import url
 from .views import view_cart, add_to_cart, adjust_cart
 
 urlpatterns = [
     path('', view_cart, name='view_cart'),
-    path('add/(?P<id>)\d+', add_to_cart, name='add_to_cart'),
-    path('adjust/(?P<id>\d+)', adjust_cart, name='adjsut_cart'),
+    url(r'^add/(?P<id>\d+)', add_to_cart, name='add_to_cart'),
+    url(r'^adjust/(?P<id>\d+)', adjust_cart, name='adjust_cart'),
 ]
--- threecart/views.py
+++ threecart/views.py
@@ -11,7 +11,7 @@ def add_to_cart(request, id):
     cart[id] = cart.get(id,quantity)
 
     request.session['cart'] = cart
-    return redirect(reverse('index'))
+    return redirect(reverse('shop'))
 
 def adjust_cart(requst, id):
     quantity = int(request.POST.get('quantity'))
