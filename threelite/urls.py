174249c57517ea82a1ee6486727ffdef413062c9
Jason Bailey <jaybailey@outlook.com>
2020-01-29 18:44:53 +0000

    added portfolio, added blog postings from blog app to portfolio.html

--- threelite/urls.py
+++ threelite/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from posts.views import get_posts
 
 urlpatterns = [
 	path('', views.index, name='index'),
+	path('portfolio/', views.portfolio, name='portfolio')
 ]
--- threelite/views.py
+++ threelite/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from posts.models import Post
+from django.utils import timezone
 
 # Create your views here.
 
 def index(request):
 	return render(request, 'threelite/index.html')
+
+def portfolio(request):
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+	return render(request, 'threelite/portfolio.html', {'posts':posts})
