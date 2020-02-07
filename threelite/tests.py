be652b257fd8d11a9c4ebaeee71d50940ac3957c
Jason Bailey <jaybailey@outlook.com>
2020-02-07 18:38:27 +0000

    Fixed error tests.py in threelite line 22

--- threelite/tests.py
+++ threelite/tests.py
@@ -19,7 +19,7 @@ class HomePageTests(SimpleTestCase):
     def test_view_uses_correct_template(self):
         response = self.client.get(reverse('index'))
         self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'index.html')
+        self.assertTemplateUsed(response, 'threelite/index.html')
 
     def test_home_page_contains_correct_html(self):
         response = self.client.get('/')
